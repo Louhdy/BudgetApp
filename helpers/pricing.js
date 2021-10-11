98c517c4a8f12c7bdb74fcc2554d54b145b3d6eb
Mel <m.gomezg@pucp.edu.pe>
2021-10-11 09:58:05 -0500

    fix: Pricing amounts for instant messages

--- helpers/pricing.js
+++ helpers/pricing.js
@@ -2,11 +2,11 @@ export const getInstanMessagePrice = (amountMessages) => {
   if (amountMessages === null) return null;
   if (amountMessages < 5000) return amountMessages*0.055;
   if ((amountMessages <= 10000) && (amountMessages > 5000)) return amountMessages*0.050;
-  if ((amountMessages <= 25000) && (amountMessages > 10000)) return amountMessages*0.050;
-  if ((amountMessages <= 50000) && (amountMessages > 25000)) return amountMessages*0.047;
-  if ((amountMessages <= 100000) && (amountMessages > 50000)) return amountMessages*0.045;
-  if ((amountMessages <= 250000) && (amountMessages > 100000)) return amountMessages*0.042;
-  if ((amountMessages <= 500000) && (amountMessages > 250000)) return amountMessages*0.040;
+  if ((amountMessages <= 25000) && (amountMessages > 10000)) return amountMessages*0.047;
+  if ((amountMessages <= 50000) && (amountMessages > 25000)) return amountMessages*0.045;
+  if ((amountMessages <= 100000) && (amountMessages > 50000)) return amountMessages*0.042;
+  if ((amountMessages <= 250000) && (amountMessages > 100000)) return amountMessages*0.040;
+  if ((amountMessages <= 500000) && (amountMessages > 250000)) return amountMessages*0.037;
   if ((amountMessages <= 1000000) && (amountMessages > 500000)) return amountMessages*0.036;
 };
 export const getProductivityAppsPrice = (amountUsers, serviceType, trackingType) => {
